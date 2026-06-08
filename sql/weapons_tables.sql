8a7f30a7c584ef82ed9a6f2dc3555ad459818522
spiderasaen <148488347+Spiderasasen@users.noreply.github.com>
2026-06-08 12:47:23 -0400

    Modify weapon tables SQL script for safety and structure

--- sql/weapons_tables.sql
+++ sql/weapons_tables.sql
@@ -1,5 +1,7 @@
 -- drops any table
-drop table Main_Weapons;
+drop table if exists Main_Weapons;
+drop table if exists Weapon_Characteristics;
+drop table if exists Weapon_Years;
 
 -- creates the tables
 create table Main_Weapons(
@@ -8,3 +10,19 @@ create table Main_Weapons(
 	description text
 );
 
+create table Weapon_Characteristics(
+	char_id serial primary key,
+	weapon_id int not null,
+	weapon_type varchar(50),
+	weapon_subtype varchar(50),
+	weapon_service varchar(100),
+	foreign key (weapon_id) references Main_Weapons(weapon_id)
+);
+
+create table Weapon_Years(
+	year_id serial primary key,
+	weapon_id int not null,
+	year_of_creation varchar(50),
+	years_of_service varchar(100),
+	foreign key (weapon_id) references Main_Weapons(weapon_id)
+);
