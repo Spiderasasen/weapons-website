5b3e179b8bd8a12188cfcc3d09f85d79c26029b5
spiderasaen <148488347+Spiderasasen@users.noreply.github.com>
2026-06-08 13:23:01 -0400

    Refactor weapon tables and add new entities
    Added new tables for Countries, Ammo, and updated Main_Weapons structure.

--- sql/weapons_tables.sql
+++ sql/weapons_tables.sql
@@ -1,28 +1,59 @@
 -- drops any table
+drop table if exists Weapon_Years;
 drop table if exists Main_Weapons;
 drop table if exists Weapon_Characteristics;
-drop table if exists Weapon_Years;
+drop table if exists Countries;
+drop table if exists Ammo;
+drop table if exists Links;
 
 -- creates the tables
-create table Main_Weapons(
-	weapon_id SERIAL primary key,
-	name varchar(100) not null,
-	description text
+-- many to one (ex. many countries made a gun, but they made that model once)
+create table Countries(
+	country_id serial primary key,
+	country_name varchar(100) not null
 );
 
 create table Weapon_Characteristics(
 	char_id serial primary key,
-	weapon_id int not null,
 	weapon_type varchar(50),
 	weapon_subtype varchar(50),
 	weapon_service varchar(100),
-	foreign key (weapon_id) references Main_Weapons(weapon_id)
+	weapon_status varchar(100)
 );
 
+create table Ammo(
+	ammo_id serial primary key,
+	ammo_type varchar(100)
+);
+
+-- main
+create table Main_Weapons(
+	weapon_id SERIAL primary key,
+	name varchar(100) not null,
+	description text,
+	-- calling all the other tables
+	country_id int,
+	char_id int,
+	ammo_id int,
+	foreign key (country_id) references Countries(country_id),
+	foreign key (char_id) references Weapon_Characteristics(char_id),
+	foreign key (ammo_id) references Ammo(ammo_id)
+);
+
+
+-- one to one relationship
 create table Weapon_Years(
 	year_id serial primary key,
 	weapon_id int not null,
 	year_of_creation varchar(50),
 	years_of_service varchar(100),
+	foreign key (weapon_id) references Main_Weapons(weapon_id)
+);
+
+create table Links(
+	link_id serial primary key,
+	weapon_id int not null,
+	image_link text,
+	soruce_link text,
 	foreign key (weapon_id) references Main_Weapons(weapon_id)
 );
