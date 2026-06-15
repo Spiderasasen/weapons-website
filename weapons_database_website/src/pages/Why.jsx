01167a502615dfbead4f4e0987714b378e65a95e
spiderasasen <spiderasasen@gmail.com>
2026-06-15 12:45:42 -0400

    introduced the idea

--- weapons_database_website/src/pages/Why.jsx
+++ weapons_database_website/src/pages/Why.jsx
@@ -1,7 +1,22 @@
+import Header from "../componets/Header.jsx";
+
 function Why() {
     return(
         <div>
-            <h1>Why I Created This?</h1>
+            <Header/>
+
+            <main className="why">
+                <h1>Why I Created This?</h1>
+                <div className="why-text">
+                    <p>Well we got 2 options and it really depends on wht you want to think</p>
+                    <ol>
+                        <li><a>I really like Weapons</a></li>
+                        <li><a>I also wanted to create something full stack</a></li>
+                    </ol>
+                </div>
+                <div id="Weapons" className={"Weapons"}></div>
+                <div id="full-stack" className={"full-stack"}></div>
+            </main>
         </div>
     )
 }
