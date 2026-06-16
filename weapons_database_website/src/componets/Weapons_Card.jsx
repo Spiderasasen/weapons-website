c7fab5c5b1be00e2e10102a4ff3f25f26fc52ce5
spiderasasen <spiderasasen@gmail.com>
2026-06-16 12:04:16 -0400

    commiting the design i have now. beacuse we are about to fight this damn thing

--- weapons_database_website/src/componets/Weapons_Card.jsx
+++ weapons_database_website/src/componets/Weapons_Card.jsx
@@ -1,9 +1,11 @@
+import "../styles/weapons_Card.css"
+
 function Weapons_Card({weapons}) {
     return(
         <div className="weapons-grid">
             {weapons.map(weapon => (
-                <div className="weapon-card" key={weapon.weapon_id}>
-                    <h2>{weapon.name}</h2>
+                <div className={`weapon-card ${weapon.weapon_type.toLowerCase()}`}>
+                <h2>{weapon.name}</h2>
                     <p><strong>Type:</strong> {weapon.weapon_type}</p>
                     <p><strong>Subtype:</strong> {weapon.weapon_subtype}</p>
                     <p><strong>Country:</strong> {weapon.country}</p>
@@ -11,8 +13,10 @@ function Weapons_Card({weapons}) {
                     <p><em>Created: {weapon.year_of_creation}</em></p>
                     <p><em>Years in Service: {weapon.years_of_service}</em></p>
                     <p>{weapon.description}</p>
-                    <a href={weapon.source_link}>Source located here</a>
-                    <img src={weapon.image_link} alt={"image of " + weapon.name}/>
+                    <a id="Weapon_link" href={weapon.source_link}>Source located here</a>
+                    <div className="image-wrapper">
+                        <img src={weapon.image_link} alt={"image of " + weapon.name} />
+                    </div>
                 </div>
             ))}
         </div>
